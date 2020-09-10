b524d57d6e3c6e701f6f630be32cad9c8294805a
KVeitch <kirk.a.veitch@gmail.com>
2020-09-10 13:21:24 -0600

    Implement internal Form functionality

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,21 +1,72 @@
 import React, {Component} from 'react'
 import './Form.css'
+import FormListItem from '../FormListItem/FormListItem'
 
 class Form extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-
-    }
+    this.state = {
+      title: "",
+      taskText:'',
+      tasks: ["bob", "susas"],
+    };
+    // this.focus = this.focus.bind(this)
   }
 
-  render(){
+  handleChange = (event) => {
+    console.log(event.target)
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleTitleEnter = (event) => {
+    if (event.keyCode === 13) {
+      this.myInp.focus()
+    }
+  };
+
+  handleTaskEnter = (event) =>{
+    if (event.keyCode === 13) {
+      this.setState({
+        tasks:[...this.state.tasks, event.target.value],
+        taskText:''
+      })
+      this.myInp.focus()
+    } 
+  }
+
+  render() {
+    const { tasks, taskText,  title } = this.state;
+    
+    const formItems = tasks.map((text, i) => (
+      <FormListItem text={text} key={`formItem${i}`} />
+    ));
 
     return (
       <div className="form">
-        <h1>Form working</h1>
+        <p>Task Title</p>
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={title}
+          autoFocus={true}
+          onChange={this.handleChange}
+          onKeyDown={this.handleTitleEnter}
+        />
+        <p>Task Items</p>
+        <ul className="list__container">{formItems}</ul>
+        <input 
+          type="text"
+          placeholder="Task Item"
+          name="taskText"
+          value={taskText}
+          ref={(ip) => this.myInp = ip}
+          onChange={this.handleChange}
+          onKeyDown={this.handleTaskEnter}
+        />
+        <button type="button">Make the thing</button>
       </div>
-    )
+    );
   }
 }
 
