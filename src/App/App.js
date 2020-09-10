f0c0e99fb92e54efed18981817506d75df633d65
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-09-10 01:08:39 -0600

    Convert App to a class component with state

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {Component}from 'react';
 import Header from '../components/Header/Header'
 import Form from '../components/Form/Form'
 import ListContainer from '../components/ListContainer/ListContainer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>App is working</h1>
-     <Header />
-     <Form />
-     <ListContainer />
-    </div>
-  );
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      lists:[]
+    }
+  };
+  render(){
+
+    return (
+      <div className="full-container">
+        <div className="App">
+          <Header />
+          <Form />
+          <ListContainer list={this.state.lists}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
